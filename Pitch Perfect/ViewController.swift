eb4eda661d1a9be0dd962e6ac50b26f19134c9e0
Lucas <lucas@agtlucas.com>
2015-02-13 15:54:01 -0200

    Updates

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
 
     @IBOutlet weak var recordingInProgress: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,13 +24,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillAppear(animated: Bool) {
+        stopButton.hidden = true
+    }
 
-    @IBAction func recordAudio(sender: UIButton) {
+
+    @IBAction func recordAudio(sender: UIButton) {                stopButton.hidden = false
         recordingInProgress.hidden = false
     }
 
     @IBAction func stopRecord(sender: UIButton) {
         recordingInProgress.hidden = true
+        stopButton.hidden = true
     }
 }
 
