a05c841ddbdc86c1fd39c1eb4b8ac207baa938b2
Lucas <lucas@agtlucas.com>
2015-02-14 14:56:50 -0200

    Update record function

--- Pitch Perfect/RecordSoundsViewController.swift
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class RecordSoundsViewController: UIViewController {
 
@@ -14,6 +15,8 @@ class RecordSoundsViewController: UIViewController {
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var recordingInProgress: UILabel!
     @IBOutlet weak var stopButton: UIButton!
+
+    var audioRecorder:AVAudioRecorder!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +38,28 @@ class RecordSoundsViewController: UIViewController {
         stopButton.hidden = false
         recordingInProgress.hidden = false
         recordButton.enabled = false
+        // Dir path
+        let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+
+        // Get date
+        var currentDateTime = NSDate()
+        // Format date
+        var formatter = NSDateFormatter()
+        formatter.dateFormat = "ddMMyyyy-HHmmss"
+        // Setup file name
+        var recordingName = formatter.stringFromDate(currentDateTime)+".wav"
+        var pathArray = [dirPath, recordingName]
+        let filePath = NSURL.fileURLWithPathComponents(pathArray)
+        println(filePath)
+
+        // Setup record session
+        var session = AVAudioSession.sharedInstance()
+        session.setCategory(AVAudioSessionCategoryPlayAndRecord, error: nil)
+        // Initialize and prepare the recorder
+        audioRecorder = AVAudioRecorder(URL: filePath, settings: nil, error: nil)
+        audioRecorder.meteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
     }
 
     @IBAction func stopRecord(sender: UIButton) {
